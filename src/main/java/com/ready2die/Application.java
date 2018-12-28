af3c476f7768e6b47c42ddd15d84dfe05fd57daa
Leesunghee <realwunz@gmail.com>
2018-12-28 22:05:10 +0900

    execute spring boot without web setting

---
+++ src/main/java/com/ready2die/Application.java
@@ -0,0 +1,23 @@
+package com.ready2die;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+//@SpringBootApplication
+
+@SpringBootConfiguration
+@ComponentScan
+//@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+
+//        SpringApplication.run(Application.class, args);
+    }
+}
