6760b749ff396a2ef35460a7bb6c05d4356c7d02
Leesunghee <realwunz@gmail.com>
2018-12-29 16:30:39 +0900

    setting tomcat in spring-boot

--- src/main/java/com/ready2die/Application.java
+++ src/main/java/com/ready2die/Application.java
@@ -1,25 +1,43 @@
 package com.ready2die;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
 
-//        SpringApplication.run(Application.class, args);
+        Context context = tomcat.addContext("/", "/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("<html><head><title>");
+                writer.println("Hey, Tomcat");
+                writer.println("</title></head>");
+                writer.println("<body><h1>Hello Tomcat</h1></body>");
+                writer.println("</html>");
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servletName, servlet);
+
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
+
     }
-
-//    @Bean
-//    public Holoman holoman() {
-//        Holoman holoman = new Holoman();
-//        holoman.setName("subok");
-//        holoman.setHowLong(60);
-//        return holoman;
-//    }
 }
