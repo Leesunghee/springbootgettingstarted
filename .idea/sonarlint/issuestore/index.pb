a23e9b45e61fa4acb81c81180f6c7baec6b424be
Leesunghee <realwunz@gmail.com>
2018-12-30 11:20:45 +0900

    setting banner using banner.txt or programming and executing Spring boot application using SpringApplicationBuilder class

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+]
+-src/main/java/com/ready2die/PortListener.java,9/b/9bbeb7d36e14cb90b5ed2ef1d54e7835686bd509
+\
+,src/main/java/com/ready2die/Application.java,a/4/a4ef6d7df63a0db35f999903fff063a5ceb72a07
--- src/main/java/com/ready2die/Application.java
+++ src/main/java/com/ready2die/Application.java
@@ -1,13 +1,52 @@
 package com.ready2die;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
+@RestController
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.run(args);
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
     }
+
+    public static void main(String[] args) {
+//        SpringApplication application = new SpringApplication(Application.class);
+//        application.setBanner(new Banner() {
+//            @Override
+//            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+//                out.println("===========================");
+//                out.println("===========================");
+//                out.println("===========================");
+//            }
+//        });
+////        application.setBannerMode(Banner.Mode.OFF);
+//        application.run(args);
+
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .run(args);
+    }
+
+//    @Bean
+//    public ServletWebServerFactory serverFactory() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8080);
+//        return connector;
+//    }
 }
