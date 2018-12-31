c6f65b57f7da097e5bac6f410b1948173a725411
Leesunghee <realwunz@gmail.com>
2018-12-31 22:10:36 +0900

    Slice test using @WebMvcTest

--- src/test/java/com/ready2die/SampleControllerTest.java
+++ src/test/java/com/ready2die/SampleControllerTest.java
@@ -3,31 +3,31 @@ package com.ready2die;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
-
-    @Autowired
-    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("captain");
-
-        webTestClient.get().uri("/hello").exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hello captain");
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello captain"));
     }
 }
