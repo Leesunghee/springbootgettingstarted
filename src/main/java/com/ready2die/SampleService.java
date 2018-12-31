cc05c3b0b2216c11464d33f89d16815ee1febaee
Leesunghee <realwunz@gmail.com>
2018-12-31 21:46:51 +0900

    test webEnvironment with MOCK

---
+++ src/main/java/com/ready2die/SampleService.java
@@ -0,0 +1,11 @@
+package com.ready2die;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "sunghee";
+    }
+}
